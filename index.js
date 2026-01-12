6932e4e999732360a587d3d2ababbd8f23e88d63
Artur <artur.perepelytsia@hive.com>
2026-01-12 15:10:39 +0200

    init

---
+++ index.js
@@ -0,0 +1,4 @@
+import fs from "fs";
+// load ./node_modules/@audi2014/npmjs-test/src/index.ts
+const text = fs.readFileSync("./node_modules/@audi2014/npmjs-test/src/index.ts", "utf-8");
+console.log(text);
